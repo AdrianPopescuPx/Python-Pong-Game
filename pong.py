d647aaf5b9390bf7cf74d62e92f754ce3adcccc2
Adrian Popescu <116150269+AdrianPopescuPx@users.noreply.github.com>
2022-12-04 13:54:02 +0200

    Created Background

---
+++ pong.py
@@ -0,0 +1,23 @@
+# Pong game in Phyton 3
+# By AdrianPopescuPx
+
+import turtle
+
+window = turtle.Screen()
+window.title("Pong By @AdrianPopescuPx")
+window.bgcolor("#145DA0")
+window.setup(width=800, height=600)
+window.tracer(0) # it stopes the window from updating / it helps speed up my game
+
+# Paddle A
+
+
+# Paddle B
+
+# Ball
+
+
+# Main game loop
+
+while True:
+    window.update()
