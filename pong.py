2930a02ce3fbc247d0d7d1567a7b3e70f1902553
Adrian Popescu <116150269+AdrianPopescuPx@users.noreply.github.com>
2022-12-06 13:25:22 +0200

    Add files via upload

--- pong.py
+++ pong.py
@@ -9,22 +9,24 @@ window.bgcolor("#145DA0")
 window.setup(width=800, height=600)
 window.tracer(0) # it stopes the window from updating / it helps speed up my game
 
-# Paddle A
+# Score
+score_a = 0
+score_b = 0
 
+# Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("#2E8BC0")
+paddle_a.color("white")
 paddle_a.shapesize(stretch_wid= 5, stretch_len= 1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
-
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("#2E8BC0")
+paddle_b.color("white")
 paddle_b.shapesize(stretch_wid= 5, stretch_len= 1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
@@ -33,17 +35,16 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("#2E8BC0")
+ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.1
 ball.dy = 0.1
 
 # Pen
-
 pen = turtle.Turtle()
 pen.speed(0)
-pen.color("#2E8BC0")
+pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
@@ -80,7 +81,6 @@ window.onkeypress(paddle_b_down, "Down")
 
 
 # Main game loop
-
 while True:
     window.update()
 
@@ -100,10 +100,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
